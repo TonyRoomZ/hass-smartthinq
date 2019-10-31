4b0ac60c7258930cdc4b7f12119a05b9ad5808a8
root <root@a0d7b954-ssh.local.hass.io>
2019-10-31 11:32:09 -0500

    add support for dryer

--- sensor.py
+++ sensor.py
@@ -10,6 +10,7 @@ from homeassistant.const import CONF_REGION, CONF_TOKEN
 
 import wideq
 from wideq import dishwasher
+from wideq import dryer
 
 ATTR_DW_STATE = 'state'
 ATTR_DW_REMAINING_TIME = 'remaining_time'
@@ -26,6 +27,9 @@ MAX_RETRIES = 5
 
 KEY_DW_OFF = 'Off'
 KEY_DW_DISCONNECTED = 'Disconnected'
+KEY_DRY_OFF = 'Off'
+KEY_DRY_DISCONNECTED = 'Disconnected'
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -39,6 +43,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     client = wideq.Client.from_token(refresh_token, region, language)
     dishwashers = []
+    dryers = []
 
     for device_id in hass.data[KEY_SMARTTHINQ_DEVICES]:
         device = client.get_device(device_id)
@@ -53,10 +58,25 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
                 # Dishwashers are only connected when in use. Ignore
                 # NotConnectedError on platform setup.
                 pass
+        
+        if device.type == wideq.DeviceType.DRYER:
+            base_name = "lg_dryer_" + device.id
+            LOGGER.debug("Creating new LG Dryer: %s" % base_name)
+            try:
+                dryers.append(LGDryerDevice(client, device, base_name))
+            except wideq.NotConnectedError:
+                # Dryers are only connected when in use. Ignore
+                # NotConnectedError on platform setup.
+                pass
 
+    if dryers:
+        add_entities(dryers, True)
     if dishwashers:
         add_entities(dishwashers, True)
+
     return True
+
+
 
 
 class LGDishWasherDevice(LGDevice):
@@ -187,6 +207,158 @@ class LGDishWasherDevice(LGDevice):
 
         try:
             status = self._dishwasher.poll()
+        except wideq.NotConnectedError:
+            self._status = None
+            return
+        except wideq.NotLoggedInError:
+            LOGGER.info('Session expired. Refreshing.')
+            self._client.refresh()
+            self._restart_monitor()
+            return
+
+        if status:
+            LOGGER.debug('Status updated.')
+            self._status = status
+            self._failed_request_count = 0
+            return
+
+        LOGGER.debug('No status available yet.')
+        self._failed_request_count += 1
+
+        if self._failed_request_count >= MAX_RETRIES:
+            # We tried several times but got no result. This might happen
+            # when the monitoring request gets into a bad state, so we
+            # restart the task.
+            self._restart_monitor()
+            self._failed_request_count = 0
+
+
+class LGDryerDevice(LGDevice):
+    def __init__(self, client, device, name):
+        """Initialize an LG Dryer Device."""
+
+        super().__init__(client, device)
+
+        # This constructor is called during platform creation. It must not
+        # involve any API calls that actually need the dryer to be
+        # connected, otherwise the device construction will fail and the entity
+        # will not get created. Specifically, calls that depend on dishwasher
+        # interaction should only happen in update(...), including the start of
+        # the monitor task.
+        self._dryer = dryer.DryerDevice(client, device)
+        self._name = name
+        self._status = None
+        self._failed_request_count = 0
+
+    @property
+    def state_attributes(self):
+        """Return the optional state attributes for the dryer."""
+        data = {}
+        data[ATTR_DW_REMAINING_TIME] = self.remaining_time
+        data[ATTR_DW_REMAINING_TIME_IN_MINUTES] = self.remaining_time_in_minutes
+        data[ATTR_DW_INITIAL_TIME] = self.initial_time
+        data[ATTR_DW_INITIAL_TIME_IN_MINUTES] = self.initial_time_in_minutes
+        data[ATTR_DW_RESERVE_TIME] = self.reserve_time
+        data[ATTR_DW_RESERVE_TIME_IN_MINUTES] = self.reserve_time_in_minutes
+        data[ATTR_DW_COURSE] = self.course
+        data[ATTR_DW_ERROR] = self.error
+
+        # For convenience, include the state as an attribute.
+        data[ATTR_DW_STATE] = self.state
+        return data
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        if self._status:
+          # Process is a more refined string to use for state, if it's present,
+          # use it instead.
+            return self._status.readable_process or self._status.readable_state
+        return "Off" 
+
+    @property
+    def remaining_time(self):
+        minutes = self.remaining_time_in_minutes if self._status else 0
+        return str(datetime.timedelta(minutes=minutes))[:-3]
+
+    @property
+    def remaining_time_in_minutes(self):
+        # The API (indefinitely) returns 1 minute remaining when a cycle is
+        # either in state off or complete, or process drying. Return 0
+        # minutes remaining in these instances, which is more reflective of
+        # reality.
+        if (self._status and
+            (self._status.state == dryer.DryerState.OFF or
+             self._status.state == dryer.DryerState.END)):
+            return 0
+        return self._status.remaining_time if self._status else 0
+
+    @property
+    def initial_time(self):
+        minutes = self.initial_time_in_minutes if self._status else 0
+        return str(datetime.timedelta(minutes=minutes))[:-3]
+
+    @property
+    def initial_time_in_minutes(self):
+        # When in state OFF, the dryer still returns the initial program
+        # length of the previously ran cycle. Instead, return 0 which is more
+        # reflective of the dryer being off.
+        if (self._status and
+            self._status.state == dryer.DryerState.OFF):
+            return 0
+        return self._status.initial_time if self._status else 0
+
+    @property
+    def reserve_time(self):
+        minutes = self.reserve_time_in_minutes if self._status else 0
+        return str(datetime.timedelta(minutes=minutes))[:-3]
+
+    @property
+    def reserve_time_in_minutes(self):
+        return self._status.reserve_time if self._status else 0
+
+    @property
+    def course(self):
+        if self._status:
+            if self._status.smart_course != KEY_DRY_OFF:
+                return self._status.smart_course
+            else:
+                return self._status.course
+        return KEY_DRY_OFF
+
+    @property
+    def error(self):
+        if self._status:
+            return self._status.error
+        return KEY_DRY_DISCONNECTED
+
+    def _restart_monitor(self):
+        try:
+            self._dryer.monitor_start()
+        except wideq.NotConnectedError:
+            self._status = None
+        except wideq.NotLoggedInError:
+            LOGGER.info('Session expired. Refreshing.')
+            self._client.refresh()
+
+    def update(self):
+        """Poll for dryer state updates."""
+
+        # This method is polled, so try to avoid sleeping in here. If an error
+        # occurs, it will naturally be retried on the next poll.
+
+        LOGGER.debug('Updating %s.', self.name)
+
+        # On initial construction, the dryer monitor task
+        # will not have been created. If so, start monitoring here.
+        if getattr(self._dryer, 'mon', None) is None:
+            self._restart_monitor()
+
+        try:
+            status = self._dryer.poll()
         except wideq.NotConnectedError:
             self._status = None
             return
